e4082b5e2fd8966878c7b3fa3d20720a9aa9f42d
Aiste Sabonyte <45278@longroad.ac.uk>
2014-10-06 10:27:56 +0100

    selection statement

---
+++ revision exercises task 3 and 5.py
@@ -0,0 +1,13 @@
+#Aiste Sabonyte
+#Revision exercises task 3
+
+number = int(input("please enter the number within the range of 21-29:"))
+if number <= 21:
+    print("the number you entered is too small and below the range")
+elif number >= 29:
+    print("the number you entered is too big and above the range") 
+else:
+    print("the number you entered is within the range:")
+#Extending the exercise 3 (task5)
+
+    
--- revision exercises task 4.py
+++ revision exercises task 4.py
@@ -3,8 +3,10 @@
 
 number1 = int(input("please enter the number1:"))
 number2 = int(input("please enter the number2:"))
-if number1 < number2:
+if number1 > number2:
+    print("number1 is larger")
+elif number1 < number2:
     print("number2 is larger")
 else:
-    print("number1 is larger")
-    
+    print("the number is out the range")
+#Extending exercise 4 (task 6)
